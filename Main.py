8f7b65392a70b9555380ccbe974e4329de8463a8
Mayank malviya <mayankmalviya440@gmail.com>
2026-02-17 18:46:45 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
+from PIL import Image
 
+print("Image Resizer developed by Monty")
+image_path = input("Enter image path: ")
+width = int(input("Enter new width: "))
+height = int(input("Enter new height: "))
+
+img = Image.open(image_path)
+resized_img = img.resize((width, height))
+
+resized_img.save("resized_image.png")
+
+print("Image resized successfully")
